8a766c7aa1a583a0971381c573356dc29ab85abc
Connor Tyrrell <connor@brewgene.com>
2024-12-14 23:07:21 -0500

    Added GH Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/story-sage",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
