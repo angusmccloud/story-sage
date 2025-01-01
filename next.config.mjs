c6078ad1a10618614355e2af259238a551ea61c3
Chris Patten <chris.patten@slalom.com>
2025-01-01 14:35:28 -0500

    conditional basePath based on environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/story_sage_frontend",
+  basePath: process.env.NODE_ENV === 'production' ? "/story_sage_frontend" : "",
   output: "export",  // <=== enables static exports
   // reactStrictMode: true,
   images: {
