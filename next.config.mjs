3d54e094520096adac5c2c55fc2b8b140664e311
Chris Patten <chris.patten@slalom.com>
2025-01-01 17:37:54 -0500

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: isProd ? "/story_sage_frontend" : "",
-  assetPrefix: isProd ? "/story_sage_frontend/" : "",
+  basePath: "/story_sage_frontend",
+  assetPrefix: "/story_sage_frontend/",
   output: "export",  // <=== enables static exports
   // reactStrictMode: true,
   images: {
